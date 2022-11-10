82a5121712482a509f25d694c13ff90250b5146b
Mikhail Gulak <miha.gulak@gmail.com>
2022-11-10 23:45:42 +0200

    add aria-expanded check in onclick handler function

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,16 @@
-document.getElementById("header").onclick = function () {
-    document.getElementById("header").innerHTML = "▼Ряди в математичному аналізі";
-}
+let header = document.getElementById("header");
+
+header.onclick = function () {
+  // зберігає значення aria-expanded  у змінну
+  let isExpanded = header.getAttribute("aria-expanded");
+
+  if (isExpanded == "true") {
+    header.innerHTML = "▶Ряди в математичному аналізі";
+    // змінює значення  aria-expanded з true  на false
+    header.setAttribute("aria-expanded", "false");
+  } else {
+    // змінює значення aria-expanded з false на true
+    header.innerHTML = "▼Ряди в математичному аналізі";
+    header.setAttribute("aria-expanded", "true");
+  }
+};
